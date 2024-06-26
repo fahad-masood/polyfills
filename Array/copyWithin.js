60f6eb1f89707a328cade8145b32b55eecfb34ce
Fahad <fahadmasood03@gmail.com>
2024-06-26 21:40:55 +0530

    Add comments to copyWithin polyfill

--- Array/copyWithin.js
+++ Array/copyWithin.js
@@ -7,9 +7,12 @@
 const arr = [0, 1, 2, 3, 4];
 
 Array.prototype.customCopyWithin = function (target, start, end) {
+  // For negative start value and undefined end, value of end would be equal to array's length
   if (start < 0 && end === undefined) {
     end = this.length;
   }
+
+  // Modify the negative indexes to a positive number
   if (target < 0) {
     target = target + this.length;
   }
@@ -20,13 +23,19 @@ Array.prototype.customCopyWithin = function (target, start, end) {
     end = end + this.length;
   }
 
+  // If the index is positive, and end is not defined, modify the value of target
   if (!end) {
     this[target] = this[start];
   }
+
+  // Iterate through the loop and check that target must always be less than array's length or it will add extra elements in the array
   for (let i = start; i < end && target < this.length; i++) {
+    // Modify the target values from start till end(not inclusive)
     this[target] = this[i];
     target++;
   }
+
+  // Return the array with modifications
   return this;
 };
 
